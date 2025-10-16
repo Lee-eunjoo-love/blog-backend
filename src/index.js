19d635c19f2bfb2778a861472a73c8b34b64e01b
Lee-eunjoo-love <hychunjae2025@gmail.com>
2025-10-16 11:44:21 +0900

    [koa-router] 라우터 설정

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 
 // #. Koa 애플리케이션은 미들웨어((ctx, next) => {})의 배열로 구성되어 있음. app.use()를 사용해 등록된 순서대로 미들웨어 실행.
 const app = new Koa();
+const router = new Router();
 
 // #. app.use() : 미들웨어 함수를 애플리케이션에 등록. ctx(Context): 웹 요청/응답 정보, next: 현재 처리중인 미들웨어 다음에 호출할 미들웨어(Promise 를 반환하며 Promise 는 다음에 처리해야 할 미들웨어가 끝나야 완료됨).
 /*app.use((ctx, next) => {
@@ -34,10 +36,30 @@ app.use((ctx, next) => {
   next();
 });
 
-app.use((ctx) => {
+/*app.use((ctx) => {
   console.log(3);
   ctx.body = "hello world!!!";
+});*/
+
+// #. 라우터 설정 : router.get("라우트 경로", "적용할 미들웨어 함수") :
+//                HTTP 메서드 : get/post/put/delete
+router.get("/", (ctx) => {
+  ctx.body = "홈";
 });
+
+// #. 파라미터 : 카테고리나 고유식별자 등의 특정 데이터 조회시 사용
+router.get("/about/:name", (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name} 의 소개` : "소개";
+});
+
+// #. 쿼리 : 옵션 관련 정보
+router.get("/posts", (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `포스트 #${id}` : "포스트 아이디가 없습니다.";
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log("Listening to port 4000");
